193618acb192dd88e1881e80a304d5ac53f19676
mharoot <michaelharootoonyan@yahoo.com>
2018-11-11 14:27:50 -0800

    finalized

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     function run() {
         add("v-2.0.1-dd-file-intake-engine-activity-diagram", "activity-diagrams/dd-file-intake/engine/v-2.0.1.svg");
         add("v-2.0.0-dd-file-intake-engine-activity-diagram", "activity-diagrams/dd-file-intake/engine/v-2.0.0.svg"); // html doesnt work
+        add("product-backlog", "GP-Color-Imaging-Group-Sprint-1/Product Backlog.html");
+        // add("sprint-1", "GP-Color-Imaging-Group-Sprint-1/Sprint 17-1.html");// jacks up my styles when i put two of these in damit
     }
     
     function add(elementID, file_url) {
@@ -22,7 +24,7 @@ $(document).ready(function(){
                 url: file_url, // "script.php",
                 method: "GET", // "POST",
                 // data: { id : menuId },
-                dataType: "html" // we are expecting html back text works for svg as well...
+                dataType: "html" // we are expecting html back text works for svg as well but if we want to append javascript use html
               });
         }
     }
